8023c8ffeeb765d19d2ec35d8ae7410fa6ff7bf7
Alina200127 <alina200127amaliy@gmail.com>
2025-01-13 21:17:49 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        <div class="header__container">
+            <h1 class="header__title">Happy Birthday</h1>
+            <h3 class="header__subtitle">Полина</h3>
+            <p class="header__text">Разрешите Вас Отхепибёздить</p>
+        </div>
+    </header>
+</template>
+
+<script setup></script>
+
+<!-- scoped - чтобы стили применялись именно к этому компоненту, а не к другим где так же могут быть
+  классы с такими же именами -->
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,111 @@
+<template>
+    <section class="section__one">
+        <div class="container">
+            <h2 class="main__title"><span>Немножко о имениннице</span></h2>
+            <div class="box__video">
+                <video class="video__end" controls>
+                    <source src="../assets/video/dr_1.mp4" type="video/mp4" />
+                </video>
+            </div>
+            <div class="box__video">
+                <video class="video__end" controls>
+                    <source src="../assets/video/бьёнсе.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </div>
+    </section>
+    <section class="section__two">
+        <div class="container">
+            <h2 class="main__title"><span>Поздравления</span></h2>
+            <div class="box">
+                <p class="box__text">
+                    Поздравляю с Днём Варенья!<br />
+                    Желаю в общем, ограбленья -<br />
+                    Чтоб на тебя напало <strong>счастье</strong>, <br />
+                    Украло все твои ненастья.
+                </p>
+                <img class="box__img" src="../assets/images/aHHmAzV.jpeg" alt="" />
+            </div>
+            <div class="box">
+                <img class="box__img" src="../assets/images/lEYXFS5.jpeg" alt="" />
+                <p class="box__text">
+                    С Днём Рождения, красотка.<br />
+                    С Днём Рождения, звезда!<br />
+                    Поздравляю и желаю много-много<br />
+                    счастья я!
+                </p>
+            </div>
+            <div class="box">
+                <p class="box__text">
+                    Позитива и добра, <br />
+                    Пусть везёт тебе всегда, <br />
+                    Много денег, жить без бед, <br />
+                    Долгих и счастливых лет!
+                </p>
+                <img class="box__img" src="../assets/images/LCpAJJK.jpeg" alt="" />
+            </div>
+            <div class="box">
+                <img class="box__img" src="../assets/images/IMG_2913.JPG" alt="" />
+                <p class="box__text box__text-frind">
+                    Чеботина, поздравляю вас с днём рождения!🕊️❤️<br />
+                    Пенсия уже не за горами у вас, но я вам желаю,<br />
+                    чтобы у вас не ломались колени, <br />
+                    всегда были деньги на чекушку и не только и обязательно<br />
+                    крепких душевных связей с любимыми людьми ❤️<br />
+                    Пусть этот год вашей жизни принесет вам <br />
+                    больше хорошего и положительных впечатлений!🕊️<br />
+                    Иногда придётся потерпеть, но бог велел,<br />
+                    а мы терпим, а потом обязательно будем радоваться!🕊️❤️<br />
+                    Вы всегда всё преодолете и сможете!<br />
+                    Поэтому желаю вам побольше нервов и <br />
+                    поменьше их тратить на ненужные вещи ❤️<br />
+                    <span>P.S Милена</span>
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="galery">
+        <div class="container">
+            <h2 class="main__title"><span>Фото-Галерея</span></h2>
+            <p>Нажми на фотки:)</p>
+            <div class="slider" id="slider">
+                <div class="page">
+                    <img src="../assets/images/slide_img/background.JPG" alt="slide 1" />
+                    <img src="../assets/images/slide_img/IMG_2766.PNG" alt="slide 2" />
+                    <img src="../assets/images/slide_img/IMG_2767.PNG" alt="slide 3" />
+                </div>
+                <div class="page">
+                    <img src="../assets/images/slide_img/IMG_2774.PNG" alt="slide 4" />
+                    <img
+                        src="../assets/images/slide_img/2025-01-03 13-58-50 (12).JPG"
+                        alt="slide 5"
+                    />
+                    <img
+                        src="../assets/images/slide_img/2025-01-03 13-58-50 (17).JPG"
+                        alt="slide 6"
+                    />
+                </div>
+                <div class="page">
+                    <img
+                        src="../assets/images/slide_img/2025-01-03 13-58-50 (10).JPG"
+                        alt="slide 1"
+                    />
+                    <img src="../assets/images/slide_img/IMG_2758.JPG" alt="slide 2" />
+                    <img src="../assets/images/slide_img/IMG_2765.PNG" alt="slide 3" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section__three">
+        <div class="container">
+            <h2 class="main__title"><span>Видео-поздравление</span></h2>
+            <div class="box__video">
+                <video class="video__end" controls>
+                    <source src="../assets/video/dr_2.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup></script>
